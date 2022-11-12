365d5de337ad774c7323f121d3672b39f21f9b3a
KKrokhaleva <kkrohaleva93@gmail.com>
2022-11-12 23:58:19 +0200

    added recipeStart component and resolver

---
+++ src/app/recipe/recipeResolver.service.ts
@@ -0,0 +1,24 @@
+import {ActivatedRouteSnapshot, Resolve, RouterStateSnapshot} from "@angular/router";
+import {Recipe} from "./recipe.model";
+import {DataStorageService} from "../shared/data-storage.service";
+import {Injectable} from "@angular/core";
+import {RecipeService} from "./recipe.service";
+
+
+@Injectable({providedIn: 'root'})
+export class RecipeResolverService implements Resolve<Recipe[]> {
+
+  constructor(private dataStorage: DataStorageService,
+                    private recipeService: RecipeService) {
+  }
+
+  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+    const recipes = this.recipeService.getRecipes();
+
+    if(recipes.length === 0) {
+      return this.dataStorage.fetchRecipes();
+    } else {
+      return recipes;
+    }
+  }
+}
